523ba85983f3a444cf8403ebe74b7470c4381a1d
eutkin <utkinev2014@gmail.com>
2018-08-05 12:52:44 +0300

    fix properties

--- src/main/java/net/eutkin/redirect/repository/LogRepository.java
+++ src/main/java/net/eutkin/redirect/repository/LogRepository.java
@@ -3,6 +3,7 @@ package net.eutkin.redirect.repository;
 import net.eutkin.redirect.entity.LogRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -17,5 +18,5 @@ import java.util.UUID;
 public interface LogRepository extends JpaRepository<LogRecord, UUID> {
 
     @Query("SELECT lr.to from net.eutkin.redirect.entity.LogRecord lr where ip = :ip order by lr.time desc")
-    Optional<LogRecord> findLastRedirectByIp(String ip);
+    Optional<LogRecord> findLastRedirectByIp(@Param("ip") String ip);
 }
