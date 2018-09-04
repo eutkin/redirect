f28fcb54c420f4578403d0d2271a8f06d9947161
eutkin <utkinev2014@gmail.com>
2018-09-04 21:51:01 +0300

    -

--- src/main/java/net/eutkin/redirect/web/RestCrudController.java
+++ src/main/java/net/eutkin/redirect/web/RestCrudController.java
@@ -1,21 +1,21 @@
 package net.eutkin.redirect.web;
 
+import lombok.extern.slf4j.Slf4j;
 import net.eutkin.redirect.entity.Parameter;
 import net.eutkin.redirect.entity.Source;
 import net.eutkin.redirect.service.crud.CrudService;
 import net.eutkin.redirect.view.ParameterView;
 import net.eutkin.redirect.view.SourceView;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api")
+@Slf4j
 public class RestCrudController {
 
     @Autowired
@@ -33,5 +33,13 @@ public class RestCrudController {
         Source source = sourceView.toEntity();
         crudService.saveSource(source);
         return ResponseEntity.ok().build();
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public String exceptionHandler(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return "Произошла ошибка";
     }
 }
