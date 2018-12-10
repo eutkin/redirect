ef388c0be044ef958407752a32a84e4edd1dc586
eutkin <utkinev2014@gmail.com>
2018-12-10 21:34:22 +0300

    добавил страницу index

--- src/main/java/net/eutkin/redirect/RedirectApplication.java
+++ src/main/java/net/eutkin/redirect/RedirectApplication.java
@@ -2,11 +2,19 @@ package net.eutkin.redirect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class RedirectApplication {
+public class RedirectApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedirectApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
 	}
 }
